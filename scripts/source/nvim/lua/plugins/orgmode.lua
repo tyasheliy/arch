28b4bc44b092c6005c2ee69ee3c84e62d8dccf36
tyasheliy <egorgerasimov@tuta.io>
2025-06-01 16:42:24 +0300

    feat: orgmode task types splitted in files

--- scripts/source/nvim/lua/plugins/orgmode.lua
+++ scripts/source/nvim/lua/plugins/orgmode.lua
@@ -5,7 +5,19 @@ return {
 		require("orgmode").setup({
 			org_agenda_files = "~/org/*",
 			org_default_notes_file = "~/org/refile.org",
-			win_split_mode = "vertical"
+			win_split_mode = "vertical",
+			org_capture_templates = {
+				m = {
+					description = "Main task",
+					template = "* TODO %?\n%U",
+					target = "~/org/main.org",
+				},
+				w = {
+					description = "Work task",
+					template = "* TODO %?\n%U",
+					target = "~/org/work.org",
+				},
+			},
 		})
 	end,
 }
