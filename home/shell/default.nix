9f380927e9b170931f7007d12e5ccf1904792037
tyasheliy <egorgerasimov@tuta.io>
2025-09-09 21:12:38 +0300

    mapping desktop env bindings to super

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -8,23 +8,20 @@
     };
     autosuggestion.enable = true;
     initExtra = ''
-      set -o allexport
-      source ${userConfig.homeDir}/.env || touch ${userConfig.homeDir}/.env
-      set +o allexport
+            set -o allexport
+            source ${userConfig.homeDir}/.env || touch ${userConfig.homeDir}/.env
+            set +o allexport
 
-      export PATH="$PATH:${userConfig.homeDir}/.config/composer/vendor/bin"
-	  export NIXPKGS_ALLOW_INSECURE=1
-	  export NIXPKGS_ALLOW_UNFREE=1
+            export PATH="$PATH:${userConfig.homeDir}/.config/composer/vendor/bin"
+      	  export NIXPKGS_ALLOW_INSECURE=1
+      	  export NIXPKGS_ALLOW_UNFREE=1
 
-	  ns() {
-		 nix shell nixpkgs#"$1" --impure
-	  }
+      	  ns() {
+      		 nix shell nixpkgs#"$1" --impure
+      	  }
     '';
-	profileExtra = ''
-		if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]; then
-			exec startx
-		fi
-	'';
+    profileExtra =
+      "	if [ -z \"$DISPLAY\" ] && [ \"$XDG_VTNR\" -eq 1 ]; then\n		exec startx\n	fi\n";
   };
 
   home.activation.installZsh = userConfig.systemInstall "zsh";
@@ -33,12 +30,12 @@
     shellAliases = {
       docker = "sudo -E docker";
       lzd = "sudo $(which lazydocker)";
-	  dcu = "docker compose up -d";
-	  dcul = "docker compose up -d && lzd";
-	  dcd = "docker compose down";
+      dcu = "docker compose up -d";
+      dcul = "docker compose up -d && lzd";
+      dcd = "docker compose down";
 
       sail = "sudo ./vendor/bin/sail";
-	  vi = "nvim";
+      vi = "nvim";
       hsw =
         "sudo echo 'Using sudo!' && export NIXPKGS_ALLOW_UNFREE=1 && home-manager switch --flake ${userConfig.cfgDir}";
       rn = userConfig.rnAlias;
