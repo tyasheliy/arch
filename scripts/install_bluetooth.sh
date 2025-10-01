e81dd0d8904a50aea00a7cb6a40aa08af322e9db
tyasheliy <egorgerasimov@tuta.io>
2025-10-01 13:38:07 +0300

    bluetooth headphones supp

--- scripts/install_bluetooth.sh
+++ scripts/install_bluetooth.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-systemInstallNew bluez bluez-utils blueman
+systemInstallNew bluez bluez-utils blueman pulseaudio-bluetooth pulseaudio-alsa
 sudo usermod -aG tyasheliy lp
 sudo systemctl enable --now bluetooth.service
-
